fb95bc185995451be680be6afa162ac1a0eea9c3
RabidRancor <aaronmk09@gmail.com>
2022-11-17 22:44:03 -0500

    bleh

--- main.c
+++ main.c
@@ -11,16 +11,26 @@ int main(){
 	text2 = fopen("d3.txt", "r");
 	text2 = fopen("d4.txt", "r");
 	
-	
+	/*must decide on how best to read and tokenize the data*/
 	while((int c = getchar())!=EOF) {
 		
 	}
 	
 	
-	/*this part would go in main after we were done reading from the original text files
+	/*this part would go in main after we were done reading from the original text files*/
 	fclose(text1);
 	fclose(text2);
 	fclose(text3);
 	fclose(text4);
+	
+	/*copied from internet:
+	
+	syntax: char *strtok(char *str, const char *delim)
+
+	Breaks string str into a series of tokens separated by delim.
+	
+	We might be able to delimit (separate) the strings read from our text files by setting the delimiter equal to spacebar
+	
+	I would also recommend visiting this link for ideas: https://www.tutorialspoint.com/c_standard_library/string_h.htm
 	*/
 }
