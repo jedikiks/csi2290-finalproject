46bcc3cc23c4af3c4eef1c6ee0b8a74c55134a0d
jedikiks <cmvogeli@proton.me>
2022-11-18 11:18:36 -0500

    added linked list data structure, w/ 3 functions

--- main.c
+++ main.c
@@ -2,26 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+typedef struct Node{
+    char *data;
+    struct Node* next;
+} Node;
+
+void insertEnd( struct Node** root, char *data, size_t size );
+void traverse( struct Node* root );
+void freeList( struct Node** root );
+
+
+int
+main(){
+
+    Node* root = malloc( sizeof( Node ) );                      // Create root node 
+    if( !root ){
+        fputs( "ERROR: Could not create root node", stderr );
+        exit( EXIT_FAILURE );
+    }
+    root->next = NULL;
 
     FILE *text1, *text2, *text3, *text4;
 	
-	text1 = fopen("d1.txt", "r");
-	text2 = fopen("d2.txt", "r");
-	text2 = fopen("d3.txt", "r");
-	text2 = fopen("d4.txt", "r");
+	text1 = fopen("../assets/d1.txt", "r");
+	text2 = fopen("../assets/d2.txt", "r");
+	text2 = fopen("../assets/d3.txt", "r");
+	text2 = fopen("../assets/d4.txt", "r");
 	
 	/*must decide on how best to read and tokenize the data*/
 	while((int c = getchar())!=EOF) {
 		
 	}
 	
-	
-	/*this part would go in main after we were done reading from the original text files*/
-	fclose(text1);
-	fclose(text2);
-	fclose(text3);
-	fclose(text4);
 	
 	/*copied from internet:
 	
@@ -33,4 +45,59 @@ int main(){
 	
 	I would also recommend visiting this link for ideas: https://www.tutorialspoint.com/c_standard_library/string_h.htm
 	*/
+
+	fclose(text1);
+	fclose(text2);
+	fclose(text3);
+	fclose(text4);
+    freeList( &root );
+    return EXIT_SUCCESS;
+}
+
+void 
+insertEnd( Node** root, char *data, size_t size ){
+    Node* newNode = malloc( sizeof( Node ) );
+        if( !newNode ){
+            fputs( "ERROR: Could not insert node", stderr );
+            exit( EXIT_FAILURE );                           // TODO: Should we exit?
+        }
+    newNode->next = NULL;
+    memcpy( newNode->data, &data, size );                    // Copy data array to new node's data
+    
+    if( !*root ){                                           // If we try to insert when root node
+        *root = newNode;                                    // is NULL, we make the pointer to
+        return;                                             // root equal to our new node.
+    }                                                       // This is why we pass Node** root
+
+    Node* currentNode = *root;                              // Iterate through linked list
+    while( !currentNode->next ){                            // until the next node is NULL
+        currentNode = currentNode->next;                    // ( this is the last one )
+    }
+
+    currentNode->next = newNode;
+}
+
+void 
+traverse( struct Node* root ){
+    Node* currentNode = root;
+    if( !currentNode ){
+        currentNode = currentNode->next;
+        traverse( currentNode );
+    }
+}
+
+void 
+freeList( struct Node** root ){
+    Node* currentNode = *root;
+                                                            // Two pointers: prevNode points to
+    Node* previousNode = currentNode;                       // node to be freed && currentNode
+    currentNode = currentNode->next;                        // moves to the next node.
+    free( currentNode->data );                              // TODO: Is this necessary?
+    free( previousNode );
+    
+    if( !currentNode ){
+        freeList( &currentNode );
+    }
+
+    *root = NULL;                                           // Implies list is empty
 }
