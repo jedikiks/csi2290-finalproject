d468c0cea7fb1bac4efe73c041f8a009d3c2d03e
RabidRancor <aaronmk09@gmail.com>
2022-11-17 22:39:10 -0500

    Added most basic of code for file reading
    do your magic

--- main.c
+++ main.c
@@ -2,8 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-int
-main(){
+int main(){
 
-    /*is this visible?*/
+    FILE *text1, *text2, *text3, *text4;
+	
+	text1 = fopen("d1.txt", "r");
+	text2 = fopen("d2.txt", "r");
+	text2 = fopen("d3.txt", "r");
+	text2 = fopen("d4.txt", "r");
+	
+	
+	while((int c = getchar())!=EOF) {
+		
+	}
+	
+	
+	/*this part would go in main after we were done reading from the original text files
+	fclose(text1);
+	fclose(text2);
+	fclose(text3);
+	fclose(text4);
+	*/
 }
