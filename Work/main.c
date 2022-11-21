d89e92ea918b5c8f3e8067ee8af24db3d34557e0
jedikiks <cmvogeli@protonmail.ch>
2022-11-21 18:59:46 -0500

    separated words into their own elements. missing 1 element

--- Work/main.c
+++ Work/main.c
@@ -24,17 +24,40 @@ int initSqlist(Sqlist* L, int maxsize)
 	return 1;
 }
 
-FILE* fp = NULL; 
+//FILE* fp = NULL; 
 Sqlist L;
+
+void getWords( FILE* file );
+void printWords();
 
 int main()
 {
 	initSqlist(&L, 1000);     //Initialize str, the size is 1000, used to store the segmented words
+	/*
 	char f1[] = "d1.txt"; //file location (your d1-d4.txt file location 
 	char f2[] = "d2.txt";
 	char f3[] = "d3.txt"; 
 	char f4[] = "d4.txt";
+	*/
 
+	FILE* d1 = fopen( "d1.txt", "r" );
+	if( !d1 )
+		perror( "couldnt open d1" );
+	FILE* d2 = fopen( "d2.txt", "r" );
+	if( !d2 )
+		perror( "couldnt open d2" );
+	FILE* d3 = fopen( "d3.txt", "r" );
+	if( !d3 )
+		perror( "couldnt open d3" );
+	FILE* d4 = fopen( "d4.txt", "r" );
+	if( !d4 )
+		perror( "couldnt open d4" );
+
+	getWords( d1 );
+	getWords( d2 );
+	getWords( d3 );
+	getWords( d4 );
+	printWords();
 /*
 	printwords(f1);
 	printwords(f2);
@@ -44,7 +67,7 @@ int main()
 	//test：
 	for (int i = 0; i < L.length; i++)
 	{
-		printf("%s\n", L.data[i].word);
+		printf("%s", L.data[i].word);
 	}
 
 	return 0;
@@ -58,47 +81,41 @@ getWords( FILE* file ){
 	char buffer[1024];
 	char stopWord[1024];
 
-	FILE* stopWordFile = fopen( "stopWords.txt", "r" );
-	fp = fopen( file, "r");
-	
-	if (fp == NULL){
+	FILE* stopWordFile = fopen( "stopwords.txt", "r" );
+	if( !stopWordFile ){
+		fprintf( stderr, "Could not open stopwords.txt\n" );
+	}
+
+	if ( file == NULL){
 		fprintf( stderr, "Could not read from file" );
 	} else{
-		while (fgets( buffer, ARRAYSIZE( buffer ) , fp) != NULL){
+		while (fgets( buffer, ARRAYSIZE( buffer ) , file ) != NULL){
 			strcpy( string, buffer ); 
-			while( !word ){											// Copy each word if
-				word = strtok( NULL, " " );								// its not a stop word 
+			word = strtok( string, " " );								// its not a stop word 
+			while( word != NULL ){											// Copy each word if
 				if( strcmp( word, fgets( stopWord, ARRAYSIZE( stopWord ), stopWordFile ) ) != 0 ){	// into the array
 						strcpy( L.data[L.length].word, word );
 						L.length++;
 				}
+				word = strtok( NULL, " " );								// its not a stop word 
 			}
 		}
 	}
+
+	fclose( file );
 }
 
-int printwords(fileofchoice) 
-{
-	char arr[1024] = {0};
-	fp = fopen(fileofchoice, "r");
-	if (fp == NULL)  //Determine whether reading the file is successful
+void printWords(){
+	for( int i = 0; i < L.length; i++ )
 	{
-		printf("file read failed");
-		return 1;
-	}
-	else
-	{
-		while (fgets(arr, 1013, fp) != NULL) //Read the content of each line in a loop, the first line of di.txt exceeds 1024bit, so it is read twice, and the others are divided once
+		char* p = " `~1!2@3#4$5%6^7&8*9(0)-_=+[{]}\\|;:',<.>/?¦©・";  //collection of delimiters
+		char buf[1024] = { 0 };
+		strcpy( buf, L.data[i].word );    //Copy to the buf array and operate on the buf array
+		char* ret = NULL;
+		for (ret = strtok(buf, p); ret != NULL; ret = strtok(NULL, p))  //Use strtok for loop segmentation operation
 		{
-			char* p = " `~1!2@3#4$5%6^7&8*9(0)-_=+[{]}\\|;:',<.>/?¦©・";  //collection of delimiters
-			char buf[1024] = { 0 };
-			strcpy(buf, arr);    //Copy to the buf array and operate on the buf array
-			char* ret = NULL;
-			for (ret = strtok(buf, p); ret != NULL; ret = strtok(NULL, p))  //Use strtok for loop segmentation operation
-			{
-				strcpy(L.data[L.length].word, ret);  //Store it in the str structure
-				L.length++;
-			}
+			//printf( "%s", ret );
+			strcpy( L.data[i].word, ret );  //Store it in the str structure
 		}
 	}
 }
